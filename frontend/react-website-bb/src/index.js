c0b5b2f731f38f96223901bbcc171c8ab0f89cda
Richard-Carnivale <145724604+Richard-Carnivale@users.noreply.github.com>
2024-08-05 23:48:25 -0400

    Update index.js

--- frontend/react-website-bb/src/index.js
+++ frontend/react-website-bb/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './app.css'; // Ensure styles are applied
+import './App.css'; // Ensure styles are applied
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
